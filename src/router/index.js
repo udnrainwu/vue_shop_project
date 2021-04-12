25fccb61ed54d66e97a0266632785ec1dec6a0b9
吳家榮 <>
2021-04-12 15:48:23 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '@/components/Login.vue'
+import Home from '@/components/Home.vue'
+import Users from '@/components/user/Users.vue'
+import Welcome from '@/components/Welcome.vue'
+import Rights from '@/components/power/Rights.vue'
+import Roles from '@/components/power/Roles.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', redirect: 'login' },
+  { path: '/login', component: Login },
+  { path: '/home', component: Home, 
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome},
+      { path: '/users', component: Users },
+      { path: '/rights', component: Rights },
+      { path: '/roles', component: Roles },
+  ] }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+  const tokenString = window.sessionStorage.getItem('token')
+  if (!tokenString) {
+    return next('/login')
+  }
+  next()
+})
+
+export default router
